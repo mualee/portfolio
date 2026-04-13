8ca8958910b5f7d21f0d67fb44c28287f66c0354
tie-ssmi <dowbib06@gmail.com>
2026-04-13 14:13:52 +0700

    ff4

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, lazy } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
